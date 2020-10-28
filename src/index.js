532f7958c6b9ab0008eb2d1c55f3b298973f9b17
Sanders Gutierrez <ing.sanders@gmail.com>
2020-10-28 07:29:02 -0300

    Signup ready

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ const config = require('./config')
 
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
     res.json({
         text: 'API works!',
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,20 @@
 'use strict'
 
+const jwt = require('jsonwebtoken')
 const { readFileSync } = require('fs')
 const { Router } = require('express')
 
 const router = Router()
+
+const users = []
+
+router.post('/signup', async (req, res) => {
+    const { username, password } = req.body
+    users.push({ username, password })
+    const newUser = users.find((user) => user.username === username)
+    const token = await jwt.sign({ username: newUser.username }, 'MY_SECRET')
+    res.status(201).json({ token })
+})
 
 router.get('/posts', (req, res) => {
     const posts = JSON.parse(readFileSync(`${process.cwd()}/data/posts.json`, 'utf-8'))
